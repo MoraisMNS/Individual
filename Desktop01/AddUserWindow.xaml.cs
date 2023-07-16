fe60daea2edac35c7f74e9ea836fa6ea1b7f713b
MoraisMNS <139705742+MoraisMNS@users.noreply.github.com>
2023-07-17 01:27:29 +0530

    Desktop_UI_WPF

---
+++ Desktop01/AddUserWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop01
+{
+    /// <summary>
+    /// Interaction logic for AddUserWindow.xaml
+    /// </summary>
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow(AddUserVM vm)
+
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.CloseAction = () => Close();
+        }
+
+        private void backbtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void cancelbtn_Click(object sender, RoutedEventArgs e)
+        {
+            fnametxt.Text = string.Empty;
+            lnametxt.Text = string.Empty;
+
+            dobtxt.Text = string.Empty;
+            int intValue;
+            if (int.TryParse(agetxt.Text, out intValue))
+            {
+                agetxt.Text = "0";
+            }
+            else
+            {
+                agetxt.Text = string.Empty;
+            }
+
+            double doubleValue;
+            if (double.TryParse(gpatxt.Text, out doubleValue))
+            {
+                gpatxt.Text = "0.0";
+            }
+            else
+            {
+                gpatxt.Text = string.Empty;
+            }
+
+
+
+
+            /*private void GpaText_TextChanged(object sender, TextChangedEventArgs e)
+            {
+                if (!char.IsDigit(GpaText.Text, GpaText.Text.Length - 1) && GpaText.Text != ".")
+                {
+                    e.Handled = true;
+                }
+            }*/
+        }
+    }
+}
+     
+    
+
---
+++ Desktop01/MainWindowVM.cs
@@ -0,0 +1,128 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01
+{
+    public  partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public  ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selectedUser=null;
+
+        [ObservableProperty]
+        public ObservableCollection<User2> users2;
+        [ObservableProperty]
+        public User2 selectedUser2 = null;
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+
+
+        [RelayCommand]
+        public void messeag()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA VALUE MUST BE BETWEEN 0 AND 4.", "ERROR");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"{name} is deleted successfully.", "DELETED \a ");
+                
+            }
+            else
+            {
+                MessageBox.Show("PLEASE SELECT A STUDENT TO DELETE.", "ERROR");
+
+
+            }
+        }
+        [RelayCommand]
+        public void ExecuteEditStudentCommand()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+
+               
+                var window = new AddUserWindow(vm);
+                
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("PLEASE SELECT A STUDENT", "WARNING!");
+            }
+        }
+            
+        public  MainWindowVM()
+        {
+            users = new ObservableCollection<User>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/1.png", UriKind.Relative));
+            users.Add(new User(24, "Nirasha", "Morais", "23/09/1999",image1,"Computer"," Dr.Chathura Seneviratne",3.2));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/2.png", UriKind.Relative));
+            users.Add(new User(25, "Mahesha", "Morais", "23/09/1999",image2,"Civil"," Dr. Kashyapa Madhubashith",3.4));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new User(23, "Praveen", "Shyamal", "22/12/1997", image3, " Electrical","Prof.Liam Sander",2.8));
+            BitmapImage image4= new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new User(26, "Shaveena", "Miriam", "18/1/2000", image4, "Mechanical"," Mr.Sampath Krandeniya",3.9));
+
+        }
+        [RelayCommand]
+        public void AddInfo()
+        {
+            var vm = new MainWindow2VM();
+            vm.title2 = "MORE INFORMATION";
+            MainWindow2 window = new MainWindow2(vm);
+            window.ShowDialog();
+
+            if (vm.Student2.Nationality != null)
+            {
+                users2.Add(vm.Student2);
+            }
+        }
+    }
+}
